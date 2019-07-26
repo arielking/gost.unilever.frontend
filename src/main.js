da843c14fc2d5b85911a3c270440cc16a4443d8a
ariel.fernandez <ariel.fernandez@tiemsolutions.com>
2019-07-26 10:16:34 -0400

    Sistema unilever inicial frontEnd

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL='http://localhost:53417/'
 new Vue({
   router,
   store,
