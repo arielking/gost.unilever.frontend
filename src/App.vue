f075231cba717148bf227c80b218474dbc7f2e2a
ariel.fernandez <ariel.fernandez@tiemsolutions.com>
2019-07-26 15:07:11 -0400

    actualizacion del menu v1

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
             </v-list-tile>
             <v-list-tile :to="{ name: 'categorias'}">
               <v-list-tile-action>
-                <v-icon>table_chart</v-icon>
+                <v-icon>attachment</v-icon>
               </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title>
@@ -232,7 +232,7 @@
     >
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">Sistema</span>
+        <span class="hidden-sm-and-down">Ghost System</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
@@ -246,17 +246,17 @@
         </v-slide-y-transition>
       </v-container>
     </v-content>
-    <v-footer dark height="auto">
+   <!-- <v-footer dark height="auto"> 
       <v-layout justify-center>
         <v-flex text-xs-center>
           <v-card flat tile color="primary" class="white--text">
             <v-card-text class="white--text pt-0">
-              IncanatoIT &copy;2018
+              Unilever  &copy;2019
             </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
-    </v-footer>
+    </v-footer> -->
 
   </v-app>
 </template>
